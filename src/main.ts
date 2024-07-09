811624c1b2a8ff1959bad7908d46cd73f9a72328
Dan Stump <ejstump@gmail.com>
2024-07-08 22:40:17 -0400

    extremely strange behavior

--- src/main.ts
+++ src/main.ts
@@ -18,15 +18,8 @@ const numAgents = 100000;
 const canvasScale = 1;
 
 // Initialize canvas and rendering context
-var canvas = <HTMLCanvasElement>document.getElementById("canvas");
-canvas.height = document.body.clientHeight / canvasScale;
-canvas.width = document.body.clientWidth / canvasScale;
-
-var ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
-
-// Fill screen with black
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const height = document.body.clientHeight / canvasScale;
+const width = document.body.clientWidth / canvasScale;
 
 // Initialize agents inside a circle with random position facing towards the center
 type Agent = {
@@ -39,8 +32,8 @@ const agentList: Agent[] = [];
 for (let i = 0; i < numAgents; i++) {
   const p: Vector = vCreate(Math.random() * 200, Math.random() * 2 * Math.PI);
   const center: Vector = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
+    x: width / 2,
+    y: height / 2,
   };
   agentList.push({
     position: vAdd(p, center),
@@ -48,42 +41,43 @@ for (let i = 0; i < numAgents; i++) {
   });
 }
 
-// Get index in a 1D array from a 2D vector
-const getIndex = (location: Vector) => {
+const agentLocations: number[][] = new Array();
+for (let i = 0; i < height; i++) {
+  agentLocations.push(new Array(width).fill(0));
+}
+for (let agent of agentList) {
+  // Deposit
+  agentLocations[Math.floor(agent.position.y)][
+    Math.floor(agent.position.x)
+  ] = 1;
+}
+
+const getBrightness = (location: Vector) => {
   if (
     location.x < 0 ||
-    location.x > canvas.width - 1 ||
+    location.x > width ||
     location.y < 0 ||
-    location.y > canvas.height - 1
+    location.y > height
   )
-    return -1;
-  else
-    return (
-      Math.floor(location.y) * 4 * canvas.width + Math.floor(location.x) * 4 + 3
-    );
+    return 0;
+  else return agentLocations[Math.floor(location.x)][Math.floor(location.y)];
 };
 
-const updateAgent = (agent: Agent, imgData: Uint8ClampedArray): Agent => {
+const updateAgent = (agent: Agent): Agent => {
   // Sense
   const temp = vMultiplyScalar(agent.velocity, senseDistance);
 
   const turnStrength = Math.random();
 
   // Get index of data to sense
-  let leftSenseIndex = getIndex(
-    vAdd(vRotateByAngle(temp, -senseAngle), agent.position)
-  );
-  let forewardSenseIndex = getIndex(vAdd(temp, agent.position));
-  let rightSenseIndex = getIndex(
-    vAdd(vRotateByAngle(temp, senseAngle), agent.position)
-  );
+  let leftSenseVector = vAdd(vRotateByAngle(temp, -senseAngle), agent.position);
+  let forewardSenseVector = vAdd(temp, agent.position);
+  let rightSenseVector = vAdd(vRotateByAngle(temp, senseAngle), agent.position);
 
   // Get value, if index is out of bounds avoid it
-  const penalty = 256;
-  const leftVal = leftSenseIndex == -1 ? penalty : imgData[leftSenseIndex];
-  const forewardVal =
-    forewardSenseIndex == -1 ? penalty : imgData[forewardSenseIndex];
-  const rightVal = rightSenseIndex == -1 ? penalty : imgData[rightSenseIndex];
+  const leftVal = getBrightness(leftSenseVector);
+  const forewardVal = getBrightness(forewardSenseVector);
+  const rightVal = getBrightness(rightSenseVector);
 
   // Rotate
   if (forewardVal <= leftVal && forewardVal <= rightVal) {
@@ -102,45 +96,39 @@ const updateAgent = (agent: Agent, imgData: Uint8ClampedArray): Agent => {
   agent.position = vAdd(agent.position, agent.velocity);
   // Ensure agent stays in bounds, wrap around borders
   if (agent.position.x < 0) {
-    agent.position.x += canvas.width;
+    agent.position.x += width;
   }
-  if (agent.position.x >= canvas.width) {
-    agent.position.x -= canvas.width;
+  if (agent.position.x >= width) {
+    agent.position.x -= width;
   }
   if (agent.position.y < 0) {
-    agent.position.y += canvas.height;
+    agent.position.y += height;
   }
-  if (agent.position.y >= canvas.height) {
-    agent.position.y -= canvas.height;
+  if (agent.position.y >= height) {
+    agent.position.y -= height;
   }
 
   return agent;
 };
 
-let prevTime = 1;
-
 // GPU Function to decay pixels
 const gpu = new GPU();
 const decay = gpu
-  .createKernel(function (
-    agentLocations: number[][],
-    prevImage: number[],
-    width: number,
-    decayFactor: number
-  ) {
-    let nextValue =
-      prevImage[this.thread.y * width * 4 + this.thread.x * 4] - decayFactor;
+  .createKernel(function (agentLocations: number[][]) {
+    let nextValue = 0;
     if (agentLocations[this.thread.y][this.thread.x] == 1) nextValue = 255;
     nextValue /= 256;
     this.color(nextValue, nextValue, nextValue);
   })
-  .setOutput([canvas.width, canvas.height])
+  .setOutput([width, height])
   .setGraphical(true);
 
 const rollingAvg: number[] = [];
 for (let i = 0; i < 20; i++) {
   rollingAvg.push(0);
 }
+
+let prevTime = 1;
 
 const animationLoop = (currentTime: number) => {
   requestAnimationFrame(animationLoop);
@@ -155,16 +143,15 @@ const animationLoop = (currentTime: number) => {
     sum += num;
   }
   console.log("Framerate: " + Math.round((sum * 100) / 20) / 100);
-  const curState = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
   // Update all agents
   for (let agent of agentList) {
-    agent = updateAgent(agent, curState.data);
+    agent = updateAgent(agent);
   }
 
   // TODO: Diffuse
-  const agentLocations: number[][] = new Array();
-  for (let i = 0; i < canvas.height; i++) {
-    agentLocations.push(new Array(canvas.width).fill(0));
+  for (let i = 0; i < height; i++) {
+    agentLocations[i].fill(0);
   }
   for (let agent of agentList) {
     // Deposit
@@ -173,10 +160,10 @@ const animationLoop = (currentTime: number) => {
     ] = 1;
   }
 
-  decay(agentLocations, curState.data, canvas.width, decayFactor);
+  decay(agentLocations);
   // Render
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  canvas.replaceWith(decay.canvas);
+  document.getElementById("canvas")?.replaceWith(decay.canvas);
   // canvas = decay.canvas;
   // ctx = <CanvasRenderingContext2D>(
   //   canvas.getContext("2d", { willReadFrequently: true })
